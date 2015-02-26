73584ef1bce3679be2ca0d7d72d544ac49116fe0
Scott Munson <scott@thinkcontra.com>
2015-02-26 14:11:16 -0500

    hide the admin bar via functions.php since none of the settings in the UI are effective

--- videotube-child/functions.php
+++ videotube-child/functions.php
@@ -2,5 +2,6 @@
 /**
  * write your functions here.
  */
+add_filter('show_admin_bar', '__return_false');
 require_once(get_stylesheet_directory().'/includes/C6_Video_Categories_Widgets.php');
 if( !defined('ABSPATH') ) exit;
