be91587d9bd50948bace6c7209a6ff2b3f2ac461
Scott Munson <scott@thinkcontra.com>
2015-02-27 14:05:07 -0500

    organize functions.php

--- videotube-child/functions.php
+++ videotube-child/functions.php
@@ -2,6 +2,15 @@
 /**
  * write your functions here.
  */
-add_filter('show_admin_bar', '__return_false');
+
 require_once(get_stylesheet_directory().'/includes/C6_Video_Categories_Widgets.php');
+
 if( !defined('ABSPATH') ) exit;
+
+// noop the mars_video_meta action hook
+function mars_video_meta() {}
+add_action('mars_video_meta', 'mars_video_meta', 10);
+
+// turn off the admin bar at the top of the site when logged in
+add_filter('show_admin_bar', '__return_false');
+
